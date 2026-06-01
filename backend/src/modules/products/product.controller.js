4df037b94c30c90052a4ad73de59ca1a0388a6ad
Gungdeadhi <agungadhi54@gmail.com>
2026-06-01 17:36:31 +0700

    updates routes warehouse

--- backend/src/modules/products/product.controller.js
+++
@@ -1,2 +0,0 @@
-import productService from "./product.service";
-import { createProduct } from "./product.validation.js";
--- backend/src/modules/warehouse/warehouse.routes.js
+++ backend/src/modules/warehouse/warehouse.routes.js
@@ -9,8 +9,8 @@ const router = express.Router();
 
 router.get("/", authenticate, authorizeRole("SUPER_ADMIN", "ADMIN"), warehouseController.getWarehouse);
 
-router.post("/create", authenticate, authorizeRole("SUPER_ADMIN", "ADMIN"), warehouseController.createWarehouse);
+router.post("/", authenticate, authorizeRole("SUPER_ADMIN", "ADMIN"), warehouseController.createWarehouse);
 
-router.put("/update/:id", authenticate, authorizeRole("SUPER_ADMIN", "ADMIN"), warehouseController.updateWarehouse);
+router.put("/:id", authenticate, authorizeRole("SUPER_ADMIN", "ADMIN"), warehouseController.updateWarehouse);
 
 export default router;
