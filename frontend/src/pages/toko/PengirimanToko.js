9c896c5201b37cb21ec9bae64e5ace52fab5179b
acidcuyy <keinanrasyid07@gmail.com>
2026-06-26 16:05:19 +0700

    benerin dikit

--- frontend/src/pages/toko/PengirimanToko.js
+++ frontend/src/pages/toko/PengirimanToko.js
@@ -31,6 +31,7 @@ async function requestNotifPermission() {
 }
 
 function sendBrowserNotif(title, body) {
+  if (!("Notification" in window)) return;
   if (Notification.permission === "granted") {
     new Notification(title, { body, icon: "/favicon.ico" });
   }
